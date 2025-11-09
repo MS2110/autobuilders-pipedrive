c3d42cda0096712ec4424448dcd187b4a5e0cfe5
MS2110 <108090719+MS2110@users.noreply.github.com>
2025-11-09 22:14:53 +0100

    g

--- apps/autobuilders-commissions/index.js
+++ apps/autobuilders-commissions/index.js
@@ -1,45 +1,6 @@
 const express = require("express");
 const path = require("path");
-const passport = require("passport");
-const { Strategy } = require("passport-oauth2");
-
-const api = require("./api");
-const config = require("./config");
-const User = require("./db/user");
-
-User.createTable();
-
 const app = express();
-
-passport.use(
-  "pipedrive",
-  new Strategy(
-    {
-      authorizationURL: "https://oauth.pipedrive.com/oauth/authorize",
-      tokenURL: "https://oauth.pipedrive.com/oauth/token",
-      clientID: config.clientID || "",
-      clientSecret: config.clientSecret || "",
-      callbackURL: config.callbackURL || "",
-    },
-    async (accessToken, refreshToken, profile, done) => {
-      const userInfo = await api.getUser(accessToken);
-      const user = await User.add(
-        userInfo.data.name,
-        accessToken,
-        refreshToken
-      );
-
-      done(null, user);
-    }
-  )
-);
-
-app.use(express.static(path.join(__dirname, "public")));
-app.use(passport.initialize());
-app.use(async (req, res, next) => {
-  req.user = await User.getById(1);
-  next();
-});
 
 const PORT = Number(process.env.PORT) || 3000;
 
