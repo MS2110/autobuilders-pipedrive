cef1a339ec7ed1c98fac986f0e53bda5c73c2a70
MS2110 <108090719+MS2110@users.noreply.github.com>
2025-11-22 22:17:22 +0400

    upd

--- apps/autobuilders-commissions/config.js
+++ apps/autobuilders-commissions/config.js
@@ -4,7 +4,7 @@ module.exports = {
     process.env.CLIENT_SECRET || "940ee5f5ee795030105f6d7ddb13cfb44afad190",
   callbackURL:
     process.env.CALLBACK_URL ||
-    "https://l8gs48kg8og4s48wcwwswkgc.autobuilders.cloud/auth/pipedrive/callback",
+    "https://l8gs48kg8og4s48wcwwswkgc.the-square.cloud/auth/pipedrive/callback",
   commissionFieldKey:
     process.env.COMMISSION_FIELD_KEY ||
     "1a514f40d36407ecd675c76cc539481989400ac6",
